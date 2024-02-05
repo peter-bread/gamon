a10d1922612c25cde75ebb881e2abfd5e97f9bff
Peter Sheehan <petersheehan2003@gmail.com>
2024-02-05 03:14:38 +0000

    reduced repeated code.
    still need to make sure it behaves as expected.
    may still need to implement more

--- cmd/init/init.go
+++ cmd/init/init.go
@@ -32,34 +32,8 @@ This line sets the environment variable GAM_REPO_ROOT_DIR to the absolute path o
 		// Default path
 		filepath := ""
 
-		// If path is provided
-		if len(args) > 0 {
-
-			// Trim whitespace
-			filepath = strings.TrimSpace(args[0])
-
-			// Replace multiple spaces with single space
-			filepath = strings.Join(strings.Fields(filepath), " ")
-
-			// Replace backslashes with forward slashes
-			filepath = strings.ReplaceAll(filepath, " / ", "/")
-
-			// Check for empty string
-			if filepath == "" {
-				log.Fatalf("Directory name cannot be empty")
-			}
-
-			// Check for special characters
-			if strings.ContainsAny(filepath, `\|:;*?"<>`) {
-				log.Fatalf("Directory name cannot contain special characters")
-			}
-
-			// Check for trailing dot (except for ".")
-			if strings.HasSuffix(filepath, ".") && filepath != "." {
-				log.Fatalf("Directory name cannot end with a dot")
-			}
-
-		} else {
+		// If path is not provided
+		if len(args) == 0 {
 			// Ask for path
 			fmt.Print("\nEnter a name for the new directory: ")
 			reader := bufio.NewReader(os.Stdin)
@@ -72,37 +46,11 @@ This line sets the environment variable GAM_REPO_ROOT_DIR to the absolute path o
 			if err != nil {
 				log.Fatalf("Failed to read input: %v", err)
 			}
-
-			// Trim whitespace
-			filepath = strings.TrimSpace(filepath)
-
-			// Replace multiple spaces with single space
-			filepath = strings.Join(strings.Fields(filepath), " ")
-
-			// Replace backslashes with forward slashes
-			filepath = strings.ReplaceAll(filepath, " / ", "/")
-
-			// Check for empty string
-			if filepath == "" {
-				log.Fatalf("Directory name cannot be empty")
-			}
-
-			// Check for special characters
-			if strings.ContainsAny(filepath, `\|:;*?"<>`) {
-				log.Fatalf("Directory name cannot contain special characters")
-			}
-
-			// Check for trailing dot (except for ".")
-			if strings.HasSuffix(filepath, ".") && filepath != "." {
-				log.Fatalf("Directory name cannot end with a dot")
-			}
-
+		} else {
+			filepath = args[0]
 		}
 
-		// Prepend "./" to the path if it doesn't already start with it
-		if !strings.HasPrefix(filepath, "./") {
-			filepath = "./" + filepath
-		}
+		filepath = processFilepath(filepath)
 
 		// Create directory
 		err := os.MkdirAll(filepath, os.ModePerm)
@@ -133,7 +81,152 @@ This line sets the environment variable GAM_REPO_ROOT_DIR to the absolute path o
 
 		// Print shell command to set environment variable
 		fmt.Printf("Add the following line to your .bashrc or .zshrc file:\n\n    export GAM_REPO_ROOT_DIR=\"%s\"\n", absPath)
+
+		// 		// Default path
+		// 		filepath := ""
+
+		// 		// If path is provided
+		// 		if len(args) > 0 {
+
+		// 			// Trim whitespace
+		// 			filepath = strings.TrimSpace(args[0])
+
+		// 			// Replace multiple spaces with single space
+		// 			filepath = strings.Join(strings.Fields(filepath), " ")
+
+		// 			// Replace backslashes with forward slashes
+		// 			filepath = strings.ReplaceAll(filepath, " / ", "/")
+
+		// 			// Check for empty string
+		// 			if filepath == "" {
+		// 				log.Fatalf("Directory name cannot be empty")
+		// 			}
+
+		// 			// Check for special characters
+		// 			if strings.ContainsAny(filepath, `\|:;*?"<>`) {
+		// 				log.Fatalf("Directory name cannot contain special characters")
+		// 			}
+
+		// 			// Check for trailing dot (except for ".")
+		// 			if strings.HasSuffix(filepath, ".") && filepath != "." {
+		// 				log.Fatalf("Directory name cannot end with a dot")
+		// 			}
+
+		// 		} else {
+		// 			// Ask for path
+		// 			fmt.Print("\nEnter a name for the new directory: ")
+		// 			reader := bufio.NewReader(os.Stdin)
+
+		// 			// Read input
+		// 			var err error
+		// 			filepath, err = reader.ReadString('\n')
+
+		// 			// Check for errors
+		// 			if err != nil {
+		// 				log.Fatalf("Failed to read input: %v", err)
+		// 			}
+
+		// 			// Trim whitespace
+		// 			filepath = strings.TrimSpace(filepath)
+
+		// 			// Replace multiple spaces with single space
+		// 			filepath = strings.Join(strings.Fields(filepath), " ")
+
+		// 			// Replace backslashes with forward slashes
+		// 			filepath = strings.ReplaceAll(filepath, " / ", "/")
+
+		// 			// Check for empty string
+		// 			if filepath == "" {
+		// 				log.Fatalf("Directory name cannot be empty")
+		// 			}
+
+		// 			// Check for special characters
+		// 			if strings.ContainsAny(filepath, `\|:;*?"<>`) {
+		// 				log.Fatalf("Directory name cannot contain special characters")
+		// 			}
+
+		// 			// Check for trailing dot (except for ".")
+		// 			if strings.HasSuffix(filepath, ".") && filepath != "." {
+		// 				log.Fatalf("Directory name cannot end with a dot")
+		// 			}
+
+		// 		}
+
+		// 		// Prepend "./" to the path if it doesn't already start with it
+		// 		if !strings.HasPrefix(filepath, "./") {
+		// 			filepath = "./" + filepath
+		// 		}
+
+		// 		// Remove trailing "/" from the path if it exists
+		// 		filepath = strings.TrimSuffix(filepath, "/")
+
+		// 		// Create directory
+		// 		err := os.MkdirAll(filepath, os.ModePerm)
+		// 		if err != nil {
+		// 			log.Fatalf("Failed to create directory: %v", err)
+		// 		} else {
+		// 			fmt.Printf("\nDirectory created: %s\n", filepath)
+		// 		}
+
+		// 		// Get the absolute path
+		// 		absPath, err := fp.Abs(filepath)
+		// 		if err != nil {
+		// 			log.Fatalf("Failed to get absolute path: %v", err)
+		// 		} else {
+		// 			fmt.Printf("Absolute path extracted: %s\n", absPath)
+
+		// 			// Get home directory
+		// 			homedir, err := os.UserHomeDir()
+		// 			if err != nil {
+		// 				log.Fatalf("Failed to get home directory: %v", err)
+		// 			} else {
+		// 				fmt.Printf("Home directory extracted: %s\n", homedir)
+		// 			}
+
+		// 			// Replace home directory with $HOME in the path
+		// 			absPath = strings.Replace(absPath, homedir, "$HOME", 1)
+		// 		}
+
+		// 		// Print shell command to set environment variable
+		// 		fmt.Printf("Add the following line to your .bashrc or .zshrc file:\n\n    export GAM_REPO_ROOT_DIR=\"%s\"\n", absPath)
 	},
+}
+
+func processFilepath(filepath string) string {
+	// Trim whitespace
+	filepath = strings.TrimSpace(filepath)
+
+	// Replace multiple spaces with single space
+	filepath = strings.Join(strings.Fields(filepath), " ")
+
+	// Replace backslashes with forward slashes
+	filepath = strings.ReplaceAll(filepath, " / ", "/")
+
+	// Check for empty string
+	if filepath == "" {
+		log.Fatalf("Directory name cannot be empty")
+	}
+
+	// Check for special characters
+	if strings.ContainsAny(filepath, `\|:;*?"<>`) {
+		log.Fatalf("Directory name cannot contain special characters")
+	}
+
+	// Check for trailing dot (except for ".")
+	if strings.HasSuffix(filepath, ".") && filepath != "." {
+		log.Fatalf("Directory name cannot end with a dot")
+	}
+
+	// Prepend "./" to the path if it doesn't already start with it
+	if !strings.HasPrefix(filepath, "./") {
+		filepath = "./" + filepath
+	}
+
+	// Remove trailing "/" from the path if it exists
+	filepath = strings.TrimSuffix(filepath, "/")
+
+	return filepath
+
 }
 
 func init() {
